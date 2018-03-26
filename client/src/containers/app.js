105e1e0e04e91aecfb0b5218601d8433b19d7a2f
Kyoji Kobayashi <talkaboutd@gmail.com>
2018-03-26 09:55:35 -0700

    folder structuring

---
+++ client/src/containers/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Navbar from "../components/navbar";
+import "./app.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
