3b4549200e9bf7ed927e22a87f769799cc8b9a6c
Adrian Haugsness <ahaugsness@icloud.com>
2018-03-27 23:24:45 -0500

    Changed Import Folder Structure and added SCSS Imports for 'Containers'

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from 'components/navbar/navbar'
-// import Navbar from "../components/navbar/navbar";
-import Content from 'containers/content/content'
-// import Content from "./content/content";
-import Column from 'components/column/column'
-// import Column from "../components/column/column";
+import Content from 'containers/content'
+import Column from 'components/column'
 
 class App extends Component {
   render () {
